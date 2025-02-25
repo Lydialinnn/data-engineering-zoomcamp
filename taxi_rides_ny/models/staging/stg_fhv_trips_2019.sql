c105561ed186cde413d13e8885d6cdcd78651685
linzhengyan0213@gmail.com <linzhengyan0213@gmail.com>
2025-02-25 04:47:03 +0000

    2.24 update

--- taxi_rides_ny/models/staging/stg_fhv_trips_2019.sql
+++ taxi_rides_ny/models/staging/stg_fhv_trips_2019.sql
@@ -8,6 +8,6 @@ select
     *
 from {{ source('staging', 'For_Hire_Vehicle_dataset') }}  -- Reference the source for FHV data
 where EXTRACT(YEAR FROM pickup_datetime) = 2019
-  and dispatching_base_num is null
+  and dispatching_base_num is not null
 
 
